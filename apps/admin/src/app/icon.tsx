c3cc58a965dba7b82270ed1914ca319918227b00
Lzdevmendes <lzmendestechdev@gmail.com>
2026-05-20 00:39:30 -0300

    feat: favicon circular com fundo escuro no admin

---
+++ apps/admin/src/app/icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from 'next/og'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+export const runtime = 'nodejs'
+export const size    = { width: 64, height: 64 }
+export const contentType = 'image/png'
+
+export default function Icon() {
+  const logoPath = join(process.cwd(), '..', 'store', 'public', 'logo.svg')
+  const svg      = readFileSync(logoPath)
+  const base64   = `data:image/svg+xml;base64,${svg.toString('base64')}`
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: 64,
+          height: 64,
+          borderRadius: '50%',
+          overflow: 'hidden',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: '#0B0E12',
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={base64} width={64} height={64} alt="" />
+      </div>
+    ),
+    { ...size }
+  )
+}
