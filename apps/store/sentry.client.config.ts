b157dd8c5574c11031c8dc1a1dbd5cb7add91bd5
Lzdevmendes <lzmendestechdev@gmail.com>
2026-06-01 15:37:39 -0300

    security: Sentry maskAllText e blockAllMedia ativados

--- apps/store/sentry.client.config.ts
+++ apps/store/sentry.client.config.ts
@@ -7,8 +7,8 @@ Sentry.init({
   replaysSessionSampleRate: 0.05,
   integrations: [
     Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
+      maskAllText: true,
+      blockAllMedia: true,
     }),
   ],
 })
